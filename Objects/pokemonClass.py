716fa8f4f9fa7c92a1b1cb13ca970b4838aabfb2
jackbk6 <jackbkelly6@gmail.com>
2025-02-13 11:57:33 +1300

    Initial commit

---
+++ Objects/pokemonClass.py
@@ -0,0 +1,7 @@
+class Pokemon:
+    def __init__(self, name, hearts, poffinStatus):
+        self.name = name
+        self.hearts = hearts
+        self.poffinStatus = poffinStatus
+
+    
---
+++ Objects/settingsClass.py
@@ -0,0 +1,3 @@
+class Settings:
+    def __init__(self, heartsPerDay):
+        self.heartsPerDay = heartsPerDay
